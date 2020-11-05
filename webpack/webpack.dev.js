ecdf4557a7f43ad7f73e7864253a509dba75bae8
Dmitry Yadrikhinsky <yadrone@gmail.com>
2020-11-05 17:50:05 +0300

    Generate source map

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,6 +3,7 @@ const config = require('./webpack.config');
 
 module.exports = Object.assign(config, {
     mode: 'development',
+    devtool: 'source-map',
     optimization: {
         splitChunks: {
             chunks: 'all',
